00ea15eb31209b218d0ee91a94ee77d3eb7cee30
Doriane Peresse <doriane.peresse@doctolib.com>
2025-01-17 14:45:38 +0100

    add new ingredient form

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,5 +2,21 @@ class IngredientsController < ApplicationController
 
   def index
     @ingredients = Ingredient.all
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      @ingredients = Ingredient.all
+      render :index
+    end
+  end
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :category)
   end
 end
