7029c0a8953ba1681b73f354c4848a51634b85b0
Doriane Peresse <doriane.peresse@doctolib.com>
2025-04-07 16:00:14 +0200

    Add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   before_action :authenticate_user!
+  after_action :verify_authorized, unless: :index
+  after_action :verify_policy_scoped, only: :index
 end
